22d19461f05324c66d13a810ae9604e5d3eb1710
Felix Berger <felix.enok.berger@gmail.com>
2025-12-02 08:17:27 +0100

    direnv basic

---
+++ Server/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Bun App";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell rec {
+            packages = with pkgs; [
+              bun
+              nodePackages.typescript
+              nodePackages.typescript-language-server
+            ];
+          };
+        };
+      }
+    );
+}
